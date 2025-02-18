89737ef2da6918a55b96c9da5d88d394e1e59c6d
Rakshit Malik <111422125+rakshitmalik136@users.noreply.github.com>
2025-02-19 00:22:37 +0530

    Create variable.sh
    Script using Variables

---
+++ variable.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+<<note
+This is a demo
+for vaeriables
+note
+
+read -p "Enter your name: " name
+
+echo "My name is $name"
+
+read -p "What is your name? " name2
+
+echo "My name is $name2"
